f93bc0e0b319a78d50455e9af58f82c027707e2f
can101 <can896373@gmail.com>
2023-07-13 04:51:40 +0300

    add: test coverage and CLI options

---
+++ src/components/button/button.spec.js
@@ -0,0 +1,13 @@
+describe("Button Component", () => {
+    //...
+    describe("When variant primary", () => {
+        //...
+        test("...", () => { });
+        test("...", () => { });
+    })
+    describe("When variant secondary", () => {
+        //...
+        test("...", () => { });
+        test("...", () => { });
+    })
+});
