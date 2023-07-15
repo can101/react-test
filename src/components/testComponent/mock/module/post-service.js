9e0ce7000c77bf30c6a303783223422057ecc36e
can101 <can896373@gmail.com>
2023-07-15 19:03:12 +0300

    add: spyOn

--- src/components/testComponent/mock/module/post-service.js
+++ src/components/testComponent/mock/module/post-service.js
@@ -1,12 +1,14 @@
-const axios = require('axios');
+// const axios = require('axios');
 // const axios = require('axios');
 
 const getPosts = () => {
-    return axios.get("https://jsonplaceholder.typicode.com/todos").then((resp) => resp.data);
+    // return axios.get("https://jsonplaceholder.typicode.com/todos").then((resp) => resp.data);
+    return fetch("https://jsonplaceholder.typicode.com/todos").then((resp) => resp.data);
 }
 
 const getPost = (id) => {
-    return axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`).then((resp) => resp.data);
+    return fetch(`https://jsonplaceholder.typicode.com/todos/${id}`).then((resp) => resp.data);
+    // return axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`).then((resp) => resp.data);
 }
 
 const postService = {
--- src/components/testComponent/mock/module/postService.spec.js
+++ src/components/testComponent/mock/module/postService.spec.js
@@ -3,20 +3,33 @@ import { render, renderHook, screen, act } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import postService from "./post-service"
 import { MOCK_POSTS } from "./mock";
-import axios from "axios";
+// import axios from "axios";
 
-jest.mock("axios");
-
+// jest.mock("axios");
+const fetch = jest.spyOn(window, "fetch");
 describe("post service", () => {
     it("should render bla bla", async () => {
-        const mockResponse = {
-            data: MOCK_POSTS
-        }
-        axios.get.mockResolvedValue(mockResponse);
+        // const mockResponse = {
+        //     data: MOCK_POSTS
+        // }
+        // axios.get.mockResolvedValue(mockResponse);
+        // const result = await postService.getPosts();
+        // console.log(result);
+
+        // expect(result).toHaveLength(MOCK_POSTS.length);
+        // expect(result).toMatchObject(MOCK_POSTS);
+
+        fetch.mockResolvedValue({
+            json: () => {
+                return Promise.resolve({
+                    data: MOCK_POSTS
+                })
+            }
+        })
         const result = await postService.getPosts();
-        console.log(result);
 
         expect(result).toHaveLength(MOCK_POSTS.length);
+        // expect(result).toHaveLength(MOCK_POSTS.length);
         expect(result).toMatchObject(MOCK_POSTS);
     })
 })
